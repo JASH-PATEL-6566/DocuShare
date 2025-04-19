2e0f49372bef77896662513b91c12200dd0bb7d6
JASH-PATEL-6566 <pateljash1511.com>
2025-04-19 14:10:04 -0300

    push by bash

--- main.sh
+++ main.sh
@@ -3,40 +3,50 @@
 # Run Terraform apply
 # ==========================
 
-cd ./Terraform
+# cd ./Terraform
 
-terraform init
-terraform apply -auto-approve
+# terraform init
+# terraform apply -auto-approve
 
-# Exit if apply fails
-if [ $? -ne 0 ]; then
-  echo "Terraform apply failed. Exiting..."
-  exit 1
-fi
+# # Exit if apply fails
+# if [ $? -ne 0 ]; then
+#   echo "Terraform apply failed. Exiting..."
+#   exit 1
+# fi
 
-cd ..
+# cd ..
 
-read -p "Terraform apply completed. Confirm to proceed with Docker build and push (y/n): " confirm
-if [[ "$confirm" != "y" ]]; then
-  echo "Operation cancelled by user."
-  exit 0
-fi
+# read -p "Terraform apply completed. Confirm to proceed with Docker build and push (y/n): " confirm
+# if [[ "$confirm" != "y" ]]; then
+#   echo "Operation cancelled by user."
+#   exit 0
+# fi
 
-# ==========================
-# PUSH IMAGE TO ECR
-# ==========================
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 715841365404.dkr.ecr.us-east-2.amazonaws.com
+# # ==========================
+# # PUSH IMAGE TO ECR
+# # ==========================
+# aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 715841365404.dkr.ecr.us-east-2.amazonaws.com
 
-# BACKEND
-docker buildx build \
-  --platform linux/amd64,linux/arm64 \
-  -t 715841365404.dkr.ecr.us-east-2.amazonaws.com/docushare-backend:latest \
-  --push \
-  ./backend
+# # BACKEND
+# docker buildx build \
+#   --platform linux/amd64,linux/arm64 \
+#   -t 715841365404.dkr.ecr.us-east-2.amazonaws.com/docushare-backend:latest \
+#   --push \
+#   ./backend
 
-# FRONTEND
-docker buildx build \
-  --platform linux/amd64,linux/arm64 \
-  -t 715841365404.dkr.ecr.us-east-2.amazonaws.com/docushare-frontend:latest \
-  --push \
-  ./frontend
+# # FRONTEND
+# docker buildx build \
+#   --platform linux/amd64,linux/arm64 \
+#   -t 715841365404.dkr.ecr.us-east-2.amazonaws.com/docushare-frontend:latest \
+#   --push \
+#   ./frontend
+
+# ======================
+# GITHUB PUSH
+# ======================
+
+git add .
+git commit -m "push by bash"
+git push
+
+echo "✅ Implemented successfully".
